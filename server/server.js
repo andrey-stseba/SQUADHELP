e3b8bed3696de48b509d3a51b8ac14b94058c43d
andrey-stseba <scebaandru@gmail.com>
2021-12-14 19:55:04 +0200

    add app.js

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+require('./dbMongo/mongoose');
+const controller = require('./socketInit');
+const app = require('./app');
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+server.listen(PORT, () =>
+  console.log(`Example app listening on port ${PORT}!`)
+);
+controller.createConnection(server);
