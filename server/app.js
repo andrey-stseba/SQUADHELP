a5e674d37ecc28c8d0f61234f1d46e3064fcf8b0
andrey-stseba <scebaandru@gmail.com>
2021-12-23 20:12:56 +0200

    Refactor contest routes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const router = require('./router');
+const router = require('./router.js');
 const handlerError = require('./handlerError/handler');
 
 const app = express();
---
+++ server/routers/contestRouter.js
@@ -0,0 +1,42 @@
+const { Router } = require('express');
+const basicMiddlewares = require('./../middlewares/basicMiddlewares');
+const checkToken = require('./../middlewares/checkToken');
+const contestController = require('./../controllers/contestController');
+const upload = require('./../utils/fileUpload');
+
+const contestRouter = Router();
+
+contestRouter.post(
+  '/dataForContest',
+  checkToken.checkToken,
+  contestController.dataForContest
+);
+
+contestRouter.get(
+  '/customers',
+  checkToken.checkToken,
+  contestController.getCustomersContests
+);
+
+contestRouter.get(
+  '/:contestId',
+  checkToken.checkToken,
+  basicMiddlewares.canGetContest,
+  contestController.getContestById
+);
+
+contestRouter.get(
+  '/',
+  checkToken.checkToken,
+  basicMiddlewares.onlyForCreative,
+  contestController.getContests
+);
+
+contestRouter.post(
+  '/updateContest',
+  checkToken.checkToken,
+  upload.updateContestFile,
+  contestController.updateContest
+);
+
+module.exports = contestRouter;
