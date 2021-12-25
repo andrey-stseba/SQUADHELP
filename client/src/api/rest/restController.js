1bf22a22e3b840890722485200cd82f7fc5c3cc3
andrey-stseba <scebaandru@gmail.com>
2021-12-25 14:45:02 +0200

    add chatRouter

--- client/src/api/rest/restController.js
+++ client/src/api/rest/restController.js
@@ -10,18 +10,20 @@ export const changeMark = data => http.post('users/changeMark', data);
 export const payMent = data => http.post('users/pay', data.formData);
 
 // Chat
-export const getPreviewChat = () => http.post('getPreview');
-export const getDialog = data => http.post('getChat', data);
-export const newMessage = data => http.post('newMessage', data);
-export const changeChatFavorite = data => http.post('favorite', data);
-export const changeChatBlock = data => http.post('blackList', data);
-export const getCatalogList = data => http.post('getCatalogs', data);
-export const addChatToCatalog = data => http.post('addNewChatToCatalog', data);
-export const createCatalog = data => http.post('createCatalog', data);
-export const deleteCatalog = data => http.post('deleteCatalog', data);
+export const getPreviewChat = () => http.post('chats/getPreview');
+export const getDialog = data => http.post('chats/getChat', data);
+export const newMessage = data => http.post('chats/newMessage', data);
+export const changeChatFavorite = data => http.post('chats/favorite', data);
+export const changeChatBlock = data => http.post('chats/blackList', data);
+export const getCatalogList = data => http.post('chats/getCatalogs', data);
+export const addChatToCatalog = data =>
+  http.post('chats/addNewChatToCatalog', data);
+export const createCatalog = data => http.post('chats/createCatalog', data);
+export const deleteCatalog = data => http.post('chats/deleteCatalog', data);
 export const removeChatFromCatalog = data =>
-  http.post('removeChatFromCatalog', data);
-export const changeCatalogName = data => http.post('updateNameCatalog', data);
+  http.post('chats/removeChatFromCatalog', data);
+export const changeCatalogName = data =>
+  http.post('chats/updateNameCatalog', data);
 // Contets
 export const updateContest = data => http.post('contests/updateContest', data);
 export const dataForContest = data =>
--- server/router.js
+++ server/router.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const basicMiddlewares = require('./middlewares/basicMiddlewares');
-const hashPass = require('./middlewares/hashPassMiddle');
-const userController = require('./controllers/userController');
+
 const contestController = require('./controllers/contestController');
 const checkToken = require('./middlewares/checkToken');
-const validators = require('./middlewares/validators');
-const chatController = require('./controllers/chatController');
+
 const upload = require('./utils/fileUpload');
 const contestRouter = require('./routers/contestRouter');
 const userRouter = require('./routers/userRouter');
+const chatRouter = require('./routers/chatRouter');
 
 const router = express.Router();
 
 router.use('/users', userRouter);
 router.use('/contests', contestRouter);
+router.use('/chats', chatRouter);
 
 router.get(
   '/downloadFile/:fileName',
@@ -35,47 +35,5 @@ router.post(
   basicMiddlewares.onlyForCustomerWhoCreateContest,
   contestController.setOfferStatus
 );
-
-router.post('/newMessage', checkToken.checkToken, chatController.addMessage);
-
-router.post('/getChat', checkToken.checkToken, chatController.getChat);
-
-router.post('/getPreview', checkToken.checkToken, chatController.getPreview);
-
-router.post('/blackList', checkToken.checkToken, chatController.blackList);
-
-router.post('/favorite', checkToken.checkToken, chatController.favoriteChat);
-
-router.post(
-  '/createCatalog',
-  checkToken.checkToken,
-  chatController.createCatalog
-);
-
-router.post(
-  '/updateNameCatalog',
-  checkToken.checkToken,
-  chatController.updateNameCatalog
-);
-
-router.post(
-  '/addNewChatToCatalog',
-  checkToken.checkToken,
-  chatController.addNewChatToCatalog
-);
-
-router.post(
-  '/removeChatFromCatalog',
-  checkToken.checkToken,
-  chatController.removeChatFromCatalog
-);
-
-router.post(
-  '/deleteCatalog',
-  checkToken.checkToken,
-  chatController.deleteCatalog
-);
-
-router.post('/getCatalogs', checkToken.checkToken, chatController.getCatalogs);
 
 module.exports = router;
